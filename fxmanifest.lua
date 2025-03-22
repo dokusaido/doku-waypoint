91954c5339c76e71e111d09af091b56738f4b36c
doku <89829952+dokusaido@users.noreply.github.com>
2025-03-23 00:33:35 +1100

    manifesto

---
+++ fxmanifest.lua
@@ -0,0 +1,16 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Doku'
+description 'Pause Menu and Waypoint'
+version '1.0.0'
+
+-- List any dependencies your resource requires
+dependencies {
+  'qb-core',
+  'lb-phone'
+}
+
+client_scripts {
+  'client.lua'
+}
